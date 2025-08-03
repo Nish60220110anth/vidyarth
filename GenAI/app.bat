9b3e30a08fd471e387ca857c58ff0d32d554a4b4
pgp40432-nishanth <kungfupandaunleashed@gmail.com>
2025-08-04 02:00:14 +0530

    remove genai

--- GenAI/app.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd C:\Users\PC\Desktop\Systems\vidyarth\GenAI
-python -m venv vidyarth
-call GenAI\vidyarth\Scripts\activate.bat
-pip install --no-cache-dir -r requirements.txt
-set FLASK_ENV=development 
-flask run
--- GenAI/config/__init__.py
+++
@@ -1,8 +0,0 @@
-import os
-
-env = os.getenv('FLASK_ENV', 'development')
-
-if env == 'production':
-    from .production_config import vidyarth_dbconfig, openaiconfig, folder, gnews, news_domain
-else:
-    from .development_config import vidyarth_dbconfig, openaiconfig, folder, gnews, news_domain
